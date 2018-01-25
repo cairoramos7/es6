de815910cd799abb80bc6a0c5b004405f853d33b
Cairo Ramos <cairo.ramosoliveira4@gmail.com>
2018-01-25 16:09:18 -0200

    Trabalhando com Herança

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-var list = [
-    "test", 
-    34,
-    {
-        name: "Cairo"
+class People {
+    constructor(name, email, phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
     }
-];
 
-for(var i of list) {
-    console.log(i);
+    toString() {
+        return `
+        Name: ${this.name}
+        Email: ${this.email}
+        Phone: ${this.phone}
+        `;
+    }
 }
+
+class Client extends People {
+    constructor(id, name, email, phone) {
+        super(name, email, phone);
+        this.id = id;
+    }
+
+    toString() {
+        return `
+        Id: ${this.id}
+        ${super.toString()}
+        `;
+    }
+}
+
+let test = new Client(1, "Cairo", "cairo.ramosoliveira4@gmail.com", "99 9999 9 999");
+console.log(test.toString());
